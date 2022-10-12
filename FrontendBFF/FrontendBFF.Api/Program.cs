16e04b5789c48115f2dac59f31d1608870e00104
Ricardo <Ricardoduinkerken@live.nl>
2022-10-12 13:32:40 +0200

    Add FrontendBFF sln

---
+++ FrontendBFF/FrontendBFF.Api/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FrontendBFF.Api
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
