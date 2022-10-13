64f0ce68b2ba473e5693bd008c6be83578ae408f
Ricardo <Ricardoduinkerken@live.nl>
2022-10-13 10:16:00 +0200

    Add authenticationService project

---
+++ AuthenticationService/AuthenticationService.Api/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AuthenticationService.Api
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
