505eb0db14d81b8f7edb25571d234062e1e4f8b6
Ricardo <Ricardoduinkerken@live.nl>
2022-10-13 10:28:45 +0200

    Add AuthService.Dal class lib

---
+++ AuthenticationService/AuthenticationService.Dal/AuthenticationServiceContext.cs
@@ -0,0 +1,21 @@
+﻿using AuthenticationService.Dal.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthenticationService.Dal
+{
+    public class AuthenticationServiceContext : DbContext
+    {
+        public DbSet<Account> Accounts { get; set; }
+
+        public AuthenticationServiceContext()
+        {
+            
+        }
+
+        public AuthenticationServiceContext(DbContextOptions<AuthenticationServiceContext> options) : base(options)
+        {
+            
+        }
+
+    }
+}
---
+++ AuthenticationService/AuthenticationService.Dal/Models/Account.cs
@@ -0,0 +1,9 @@
+﻿namespace AuthenticationService.Dal.Models
+{
+    public class Account
+    {
+        public long Id { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
